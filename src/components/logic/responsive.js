2916e14e5c65d15770884679ad8d91c9dff5735c
PepeM2002 <josemanuelav04@gmail.com>
2024-03-21 13:56:43 -0600

    21/03

---
+++ src/components/logic/responsive.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var logo = document.getElementById('logo')
+    var nombre = document.getElementById('nombre')
+
+    function responsive() {
+        if (window.innerWidth < 526) {
+            nombre.style.display = 'none';
+            logo.style.marginLeft = '0';
+        } else {
+            nombre.style.display = 'block';
+            logo.style.marginLeft = '8px;'
+        }
+    }
+    responsive()
+    window.addEventListener('resize', responsive)
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (mobileMenuToggle && mobileMenu) {
+        mobileMenuToggle.addEventListener('click', function () {
+            mobileMenu.classList.toggle('hidden');
+        });
+
+        function handleResize() {
+            if (window.innerWidth < 433) {
+                mobileMenu.classList.add('hidden');
+            } else {
+                mobileMenu.classList.remove('hidden');
+            }
+        }
+
+        handleResize(); 
+        window.addEventListener('resize', handleResize); 
+    } else {
+        console.error(
+            "No se encontraron los elementos con los ID 'mobile-menu-toggle' o 'mobile-menu'"
+        );
+    }
+});
