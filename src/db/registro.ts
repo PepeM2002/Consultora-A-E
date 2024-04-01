70781a3208831026c28b71338c80db4da9ebe347
PepeM2002 <josemanuelav04@gmail.com>
2024-04-01 15:12:22 -0600

    as

--- src/db/registro.ts
+++ src/db/registro.ts
@@ -20,5 +20,5 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     return new Response(error.message, { status: 500 });
   }
 
-  return redirect("/login");
+  return redirect("/login.astro");
 };
