55eb0dd9372e0f04f4a73f130db5b0e39f8334ac
PepeM2002 <josemanuelav04@gmail.com>
2024-04-01 18:44:40 -0600

    ya paro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind()] 
 });
