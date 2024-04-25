5b5fd13f93430431c41a163164cdc31f0f724a8c
PepeM2002 <josemanuelav04@gmail.com>
2024-04-25 08:39:57 -0600

    25

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: vercel()
+  adapter: cloudflare()
 });
